07f503806cafa5b7eb0afc1a5a6d8c086c7583e7
SergMicar <Sergey.V.Goncharov@gmail.com>
2020-02-17 15:19:41 +0200

    correct method call

--- Project1/venv/laba2/__init__.py
+++ Project1/venv/laba2/__init__.py
@@ -1,34 +1,21 @@
+def print_digits_sum(arg1, arg2):
+    print('Суммируем введенные числа')
+    return int(arg1) + int(arg2)
+
+
 class SumFunction:
     x1 = input('Введите x: ')
-
     if len(x1) != 3:
         print('Число не трехзначное')
     else:
+        print('Число явяется трехзначным')
         lst = list(map(int, list(x1)))
         first, last = lst[:3], lst[3:]
+        print(sum(first))
+        print(sum(last))
 
         if sum(first) == sum(last):
-            print( print_digits_sum)
+            print(print_digits_sum)
 
 
- '''
- input("Введите трехзначное число для суммирования ")
-
- print("Отлично! Вы ввели трехзначное число")
-
- input("Введите трехзначное число для суммирования ")
- print("Отлично! Вы ввели трехзначное число")
-
- print("Суммируем")
-'''
-
-#def print_digits_sum(arg1, arg2):
-#  print(x1 + x2)
-
-
-
-
-
-
-
-
+print('Завершение работы программы')
--- Project1/venv/laba2/ex1/__init__.py
+++ Project1/venv/laba2/ex1/__init__.py
@@ -1,15 +1,12 @@
 x1 = input('Введите x1: ')
-
 if len(x1) != 3:
     print('Число не трехзначное')
 else:
     lst = list(map(int, list(x1)))
     first, last = lst[:3], lst[3:]
-
     print('Отлично')
 
 x2 = input('Введите x2: ')
-
 if len(x2) != 3:
     print('Число не трехзначное')
 else:
@@ -17,11 +14,11 @@ else:
     first, last = lst[:3], lst[3:]
     print('Отлично')
 
-print('Суммируем')
+
+def print_digits_sum(m1, m2):
+    print('Вызвана функция print_digits_sum, суммируем введенный числа ' + m1 + " и " + m2)
+    return int(m1) + int(m2)
 
 
-def print_digits_sum(x1, x2):
-    return (x1 + x2)
-    result=(x1+x2)
-
-    print(result)
+# явное преобразование типа функцией str() используется т.к. конкатенация int со string невозможна в питон
+print('Результат суммирования равен ' + str(print_digits_sum(x1, x2)))
